aa4b1ec0dea6331b28b0af46aa87b30797107052
ilsyt <1756304680@qq.com>
2023-03-08 18:40:56 +0800

    Create a new file

---
+++ president_tree.cpp
@@ -0,0 +1,64 @@
+#include<cstdio>
+#include<vector>
+#include<algorithm>
+using namespace std;
+struct Node
+{
+    int sum;
+    Node* lc;
+    Node* rc;
+    static int _sum(Node* ptr){return ptr==NULL?0:ptr->sum;}
+    Node(int s=0):sum(s),lc(NULL),rc(NULL) {}
+};
+vector<Node*> _Root;
+Node* build(int l,int r)
+{
+    Node* ptr=new Node;
+    if(l>=r)return ptr;
+    int mid=(l+r)>>1;
+    ptr->lc=build(l,mid);
+    ptr->rc=build(mid+1,r);
+    return ptr;
+}
+Node* modify(int l,int r,int x,Node* nroot)
+{
+    Node* ptr=new Node(Node::_sum(nroot)+1);
+    ptr->lc=nroot->lc;
+    ptr->rc=nroot->rc;
+    if(l>=r)return ptr;
+    int mid=(l+r)>>1;
+    if(x<=mid)ptr->lc=modify(l,mid,x,nroot->lc);
+    else ptr->rc=modify(mid+1,r,x,nroot->rc);
+    return ptr;
+}
+int query(int l,int r,int q,Node* nl,Node* nr)
+{
+    if(l>=r)return l;
+    int x=Node::_sum(nr->lc)-Node::_sum(nl->lc);
+    int mid=(l+r)>>1;
+    if(q<=x)return query(l,mid,q,nl->lc,nr->lc);
+    else return query(mid+1,r,q-x,nl->rc,nr->rc);
+}
+int a[200050],b[200050];
+int main()
+{
+    int n,m;
+    scanf("%d%d",&n,&m);
+    for(int i=1;i<=n;++i)scanf("%d",a+i),b[i]=a[i];
+    sort(b+1,b+n+1);
+    int len=unique(b+1,b+n+1)-b-1;
+    _Root.push_back(NULL);
+    _Root[0]=build(1,len);
+    for(int i=1;i<=n;++i)
+    {
+        int p=lower_bound(b+1, b+len+1,a[i])-b;
+        _Root.push_back(modify(1,len,p,_Root[i-1]));
+    }
+    for(int i=0;i<m;++i)
+    {
+        int l,r,k;
+        scanf("%d%d%d", &l, &r, &k);
+        int ans=query(1,len,k,_Root[l-1],_Root[r]);
+        printf("%d\n", b[ans]);
+    }
+}
