ce4fea69155b38748975bcacdcfd2a3f0975724e
ilsyt <1756304680@qq.com>
2023-03-19 22:23:42 +0800

    Format again

--- president_tree.cpp
+++ president_tree.cpp
@@ -1,6 +1,7 @@
 #include <cstdio>
 #include <vector>
 #include <algorithm>
+
 struct Node
 {
     int sum;
@@ -9,7 +10,9 @@ struct Node
     static int _sum(Node *ptr) { return ptr == NULL ? 0 : ptr->sum; }
     Node(int s = 0) : sum(s), lc(NULL), rc(NULL) {}
 };
-std::vector<Node *> _Root;
+
+std::vector<Node *> Root;
+
 Node *build(int l, int r)
 {
     Node *ptr = new Node;
@@ -20,6 +23,7 @@ Node *build(int l, int r)
     ptr->rc = build(mid + 1, r);
     return ptr;
 }
+
 Node *modify(int l, int r, int x, Node *nroot)
 {
     Node *ptr = new Node(Node::_sum(nroot) + 1);
@@ -34,6 +38,7 @@ Node *modify(int l, int r, int x, Node *nroot)
         ptr->rc = modify(mid + 1, r, x, nroot->rc);
     return ptr;
 }
+
 int query(int l, int r, int q, Node *nl, Node *nr)
 {
     if (l >= r)
@@ -45,6 +50,7 @@ int query(int l, int r, int q, Node *nl, Node *nr)
     else
         return query(mid + 1, r, q - x, nl->rc, nr->rc);
 }
+
 int a[200050], b[200050];
 int main()
 {
@@ -54,18 +60,18 @@ int main()
         scanf("%d", a + i), b[i] = a[i];
     std::sort(b + 1, b + n + 1);
     int len = std::unique(b + 1, b + n + 1) - b - 1;
-    _Root.push_back(nullptr);
-    _Root[0] = build(1, len);
+    Root.push_back(nullptr);
+    Root[0] = build(1, len);
     for (int i = 1; i <= n; ++i)
     {
         int p = std::lower_bound(b + 1, b + len + 1, a[i]) - b;
-        _Root.push_back(modify(1, len, p, _Root[i - 1]));
+        Root.push_back(modify(1, len, p, Root[i - 1]));
     }
     for (int i = 0; i < m; ++i)
     {
         int l, r, k;
         scanf("%d%d%d", &l, &r, &k);
-        int ans = query(1, len, k, _Root[l - 1], _Root[r]);
+        int ans = query(1, len, k, Root[l - 1], Root[r]);
         printf("%d\n", b[ans]);
     }
 }
